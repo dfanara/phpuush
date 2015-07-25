a53bcd77f0cbb3ca5e0799f9d9d18c7423eefdca
Daniel Fanara <fanardat16@gmail.com>
2015-07-25 03:51:45 -0400

    Updated design. TODO: Create a more config-friendly HTML page setup.

--- controllers/file-handler.php
+++ controllers/file-handler.php
@@ -100,26 +100,22 @@ else
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
 	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
 	<style>
-	html, body {margin: 0; padding: 0; height: auto; width: 100%; background: #F9F9F9; position: relative; font-family: 'Open Sans', sans-serif; text-align: center;}
-	.img { margin-top: 30px; margin-bottom: 70px;}
-	footer {position: absolute; bottom: 10px; height: 50px; text-align: center; width: 100%;}
-	footer .lol {margin: auto; width: 100%; max-width: 960px; height: 100%; text-align: left;}
+	html, body {margin: 0; padding: 0; height: auto; width: 100%; background: #F9F9F9; position: relative; font-family: 'Open Sans', sans-serif; text-align: center; }
+	.img { margin-top: 30px; margin-bottom: 90px; }
+	footer {height: 50px; text-align: center; width: 100%; margin-top: 20px; position: absolute; bottom: 20px;}
+	a:active, a:hover, a {color: #000; text-decoration: none;}
 	@media screen and (max-width: 568px) {
-			.img {max-width: 100%; margin-top: 0;}
-			footer .lol {width: auto; text-align: center;}
+			.img {max-width: 100%; margin-top: 0; margin-bottom: 70px;}
+			footer {margin-top: 10px; bottom: 10px;}
 	}
 	</style><body>".$image."
-	<footer><div class='lol'>
-	<div style='margin: 0; display: inline-block; font-size: 0px; height: 50px;'>
-		<img src='http://pbs.twimg.com/profile_images/571147898500603905/WS3SXytK.jpeg' style='display: inline-block; height: 50px; width: 50px; border-radius: 100%;'/>
-		<div style='height: 50px; display: inline-block; font-size: 0px; padding-left: 10px; vertical-align: top;'>
-			<ul style='list-style-type: none; margin: 0; padding: 0; font-size: 0px; height: 50px;	'>
-				<li style='line-height: 22px; font-size: 16px; font-weight: 400; color: #1F1F21;'>Daniel Fanara</li>
-				<li style='font-size: 12px; line-height: 17px; font-weight: 300; font-style: italic; color: #8E8E8E;'>@fanieldanara</li>
-			</ul>
-		</div>
-	</div>
-	</div></footer>
+	<footer>
+		<img src='http://pbs.twimg.com/profile_images/571147898500603905/WS3SXytK.jpeg' style='display: inline-block; height: 50px; width: 50px; border-radius: 100%; margin-right: 10px;'/>
+		<ul style='list-style-type: none; margin: 0; padding: 0; font-size: 0px; height: 50px; display: inline-block; vertical-align: top; text-align: left;'>
+			<a href='http://fanara.me'><li style='line-height: 22px; font-size: 16px; font-weight: 400; color: #1F1F21;'>Daniel Fanara</li><a/>
+			<a href='http://twitter.com/fanieldanara'><li style='font-size: 12px; line-height: 17px; font-weight: 300; font-style: italic; color: #8E8E8E;'>@fanieldanara</li></a>
+		</ul>
+	</footer>
 	</body></html>";
 	header("Content-Length: " + strlen($sData));
 	header("Content-Type: text/html");
